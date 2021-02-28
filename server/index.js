8f169e5577572ac2e18bb09fbe26c6c99bb9b6e7
yishai <yishai5555@gmail.com>
2021-02-28 11:53:59 +0200

    07-add-greet-to-server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,22 @@
+const greets = require('../server/protos/greet_pb')
+const service = require('../server/protos/greet_grpc_pb')
+
 const grpc = require('grpc')
+
+/*
+    Implements the greet RPC method.
+*/
+
+const greet = (call, callback) => {
+    const greeting = new greets.GreetResponse()
+
+    greeting.setResult(
+        "Hellow " + call.getGreeting().getFirstname()
+    )
+
+    callback(null, greeting)
+}
+
 const main = () => {
     const server = new grpc.Server()
     // console.log({grpc, server})
