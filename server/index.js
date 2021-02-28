b23888d82ce00e7b671fee371e94d53ae3ebd2bd
yishai <yishai5555@gmail.com>
2021-02-28 11:10:40 +0200

    04-add-server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const grpc = require('grpc')
 const main = () => {
     const server = new grpc.Server()
+    console.log({grpc, server})
     const hostAndPort = "127.0.0.1:50051"
     server.bind(hostAndPort, grpc.ServerCredentials.createInsecure())
     server.start()
+    console.log({server})
 
     console.log(`Server is running on ${hostAndPort}`)
 }
