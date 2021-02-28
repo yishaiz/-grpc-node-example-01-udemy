dee8239839ae3296b1b33af8db27ff9851789388
yishai <yishai5555@gmail.com>
2021-02-28 11:24:53 +0200

    05-add-client

---
+++ client/index.js
@@ -0,0 +1,18 @@
+const grpc = require('grpc')
+
+const services = require('../server/protos/dummy_grpc_pb')
+
+const main = () => {
+    const hostAndPort = "localhost:50051"
+    const client = new services.DummyServiceClient(
+        hostAndPort,
+        grpc.credentials.createInsecure()
+    )
+
+    // here we will do studd later
+
+    console.log(`hello from client`)
+
+    console.log({ client })
+}
+main();
